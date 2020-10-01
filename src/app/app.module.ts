09140ae9fd358d2a2760c4b8b7c95b9a54465700
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-09-30 22:13:34 -0500

    some basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RuleGeneratorComponent } from './rule-generator/rule-generator.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { RuleGeneratorComponent } from './rule-generator/rule-generator.componen
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
