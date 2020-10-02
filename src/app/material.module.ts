1ff71aa6aa4afe907a8009fabfc4df3fb15b0c8f
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-10-01 20:06:22 -0500

    some more formatting and styling changes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
