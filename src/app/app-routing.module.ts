2d7d4de39bcc90a8db381b5c36496c59e00d3ccc
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-10-02 19:42:17 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { RuleFavoritesComponent } from './rule-favorites/rule-favorites.component';
 import { RuleGeneratorComponent } from './rule-generator/rule-generator.component';
 import { RuleListComponent } from './rule-list/rule-list.component';
 
 
 const routes: Routes = [
-  {path: 'list', component: RuleListComponent},
-  {path: 'favorites', component: RuleFavoritesComponent},
-  {path: '**', component: RuleGeneratorComponent}
+  { path: 'list', component: RuleListComponent },
+  { path: 'favorites', component: RuleFavoritesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: RuleGeneratorComponent }
 ];
 
 @NgModule({
