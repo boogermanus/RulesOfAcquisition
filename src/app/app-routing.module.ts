6c5d83d5ee32a97e475ccff7faa7e04445854ac5
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-12-26 17:35:33 -0600

    merge code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { RuleFavoritesComponent } from './rule-favorites/rule-favorites.component';
+import { RuleGeneratorComponent } from './rule-generator/rule-generator.component';
+import { RuleListComponent } from './rule-list/rule-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'list', component: RuleListComponent },
+  { path: 'favorites', component: RuleFavoritesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: RuleGeneratorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RuleGeneratorComponent } from './rule-generator/rule-generator.component';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+import { RuleViewComponent } from './rule-view/rule-view.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { RuleListComponent } from './rule-list/rule-list.component';
+import { RuleFavoritesComponent } from './rule-favorites/rule-favorites.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RuleGeneratorComponent,
+    RuleViewComponent,
+    NavMenuComponent,
+    RuleListComponent,
+    RuleFavoritesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
