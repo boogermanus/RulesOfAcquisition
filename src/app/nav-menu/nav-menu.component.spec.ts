e74c878d4c5ef42ebb1f5e2a72fbc4396c2d7516
Jordan Woodruff <boogermanus@gmail.com>
2025-06-24 16:35:49 -0500

    fixed tests

--- src/app/nav-menu/nav-menu.component.spec.ts
+++ src/app/nav-menu/nav-menu.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import { NavMenuComponent } from './nav-menu.component';
+import {MaterialModule} from "../material.module";
+import {provideRouter, RouterModule} from "@angular/router";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('NavMenuComponent', () => {
   let component: NavMenuComponent;
@@ -8,7 +11,9 @@ describe('NavMenuComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavMenuComponent ]
+      declarations: [ NavMenuComponent ],
+      imports: [MaterialModule, RouterModule],
+      providers: [provideRouter([])]
     })
     .compileComponents();
   }));
